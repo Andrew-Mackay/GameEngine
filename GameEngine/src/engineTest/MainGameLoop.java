901c683dc82eb2c6d627ee254c27dc9f6a5ffcff
Chimp022 <mackay-andrew@hotmail.com>
2016-12-22 19:58:08 +0000

    Completed tutorial 1.

---
+++ GameEngine/src/engineTest/MainGameLoop.java
@@ -0,0 +1,22 @@
+package engineTest;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+
+		DisplayManager.createDisplay();
+		
+		while(!Display.isCloseRequested()){
+			
+			//game logic
+			//render
+			DisplayManager.updateDisplay();
+		}
+		DisplayManager.closeDisplay();
+
+	}
+}
